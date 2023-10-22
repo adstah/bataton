6cd9cd441ba5723a593b26b68560ac2b8b12b9e3
Stachowski <adam.stachowski@capgemini.com>
2023-10-22 20:23:02 +0200

    init2

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 global.performance = global.performance || {
   now: () => new Date().getTime(),
 };
+//
 
 export const metadata: Metadata = {
   title: "Create Next App",
